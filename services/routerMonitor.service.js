24c694c8b3da96a99194b9b431bdeffb9de39d30
runjiian-arch <runjiian@gmail.com>
2026-03-10 13:34:57 +0300

    Create routerMonitor.service.js file in services directory

---
+++ services/routerMonitor.service.js
@@ -0,0 +1,9 @@
+// routerMonitor.service.js
+
+// Monitor service for the router
+
+export const monitorRouter = () => {
+    console.log('Router is being monitored');
+};
+
+// You can expand this service with additional functionalities related to monitoring the router.
